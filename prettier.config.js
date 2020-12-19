5308b96299e62066f7dbfe286ff61719fcd4b18d
Lonelygriffin <lonelygriffin@gmail.com>
2020-12-19 17:03:25 +0300

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 120,
+    singleQuote: true,
+    trailingComma: "none",
+    jsxSingleQuote: true,
+    bracketSpacing: false,
+    jsxBracketSameLine: false,
+    tabWidth: 2,
+    semi: false,
+    arrowParens: "always",
+}
