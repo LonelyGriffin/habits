f108a5fc4a8598b1cf130c4e165447c1d66c451f
Lonelygriffin <lonelygriffin@gmail.com>
2021-01-05 10:54:19 +0300

    try fix cors

--- src/common/service/yadisk/appYadisk.ts
+++ src/common/service/yadisk/appYadisk.ts
@@ -87,15 +87,11 @@ export class AppYadisk implements Yadisk {
         }
       )
 
-      if (!downloadResponse?.data?.href) {
+      if (!meta?.data?.file) {
         return new AppResultError('Sync failed')
       }
 
-      const response = await axios.get(downloadResponse?.data?.href, {
-        headers: {
-          origin: null
-        }
-      })
+      const response = await axios.get(meta?.data?.file)
 
       const data = response.data
 
@@ -127,3 +123,8 @@ export class AppYadisk implements Yadisk {
     return new AppResult()
   }
 }
+
+// https://downloader.disk.yandex.ru/disk/d319a054264a86b2c6edaf86abd634443cf89d12ee4f7292b5d79872b705da7c/5ff450b5/n8cCt5K--tPekjAr9qgsFz0xllLzyYpNe5gjedHtPHaQQu4VJmTtFoneTq9-UkiDLBjPgx7qmvO76t8jpJuanA%3D%3D?uid=174169485&filename=solid_storage.json&disposition=attachment&hash=&limit=0&content_type=text%2Fplain&owner_uid=174169485&fsize=91&hid=c8adafc8c7c5e70da81e1635eefc4b49&media_type=text&tknv=v2&etag=32f7432eca8fbc3fb73393acb9e51764
+// https://downloader.disk.yandex.ru/disk/628f87f295e30822f583bb53ac3727179b76611ace0740387d4fa80f5c7ec88f/5ff450b6/n8cCt5K--tPekjAr9qgsFz0xllLzyYpNe5gjedHtPHaQQu4VJmTtFoneTq9-UkiDLBjPgx7qmvO76t8jpJuanA%3D%3D?uid=174169485&filename=solid_storage.json&disposition=attachment&hash=&limit=0&content_type=text%2Fplain&owner_uid=174169485&fsize=91&hid=c8adafc8c7c5e70da81e1635eefc4b49&media_type=text&tknv=v2&etag=32f7432eca8fbc3fb73393acb9e51764
+// https://downloader.disk.yandex.ru/disk/6c5b7d9b9482543fd52d708622ad0db7a4396af22c082da2e33a1efe3039365b/5ff4518c/n8cCt5K--tPekjAr9qgsFz0xllLzyYpNe5gjedHtPHaQQu4VJmTtFoneTq9-UkiDLBjPgx7qmvO76t8jpJuanA%3D%3D?uid=174169485&filename=solid_storage.json&disposition=attachment&hash=&limit=0&content_type=text%2Fplain&owner_uid=174169485&fsize=91&hid=c8adafc8c7c5e70da81e1635eefc4b49&media_type=text&tknv=v2&etag=32f7432eca8fbc3fb73393acb9e51764
+// https://downloader.disk.yandex.ru/disk/b6757401ce2f3751b593cc520b990e1e2eb96044a3856db36aaec5879efb9a7a/5ff451dd/n8cCt5K--tPekjAr9qgsFz0xllLzyYpNe5gjedHtPHaQQu4VJmTtFoneTq9-UkiDLBjPgx7qmvO76t8jpJuanA%3D%3D?uid=174169485&filename=solid_storage.json&disposition=attachment&hash=&limit=0&content_type=text%2Fplain&owner_uid=174169485&fsize=91&hid=c8adafc8c7c5e70da81e1635eefc4b49&media_type=text&tknv=v2&etag=32f7432eca8fbc3fb73393acb9e51764
